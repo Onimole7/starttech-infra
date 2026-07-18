044d3c7cd9270f5667d5911f6c796bb2cf21116a
Onimole7 <onimoleolorunisola7@gmail.com>
2026-07-18 09:40:15 -0700

    Use free-tier-eligible instance type (t3.micro)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
-variable "aws_region" { default = "us-east-1" }
-variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "aws_region"      { default = "us-east-1" }
+variable "vpc_cidr"        { default = "10.0.0.0/16" }
 variable "cluster_version" { default = "1.34" }
-variable "node_instance_type" { default = "t3.medium" }
-variable "node_desired_size" { default = 2 }
+variable "node_instance_type" { default = "t3.micro" }
+variable "node_desired_size"  { default = 2 }
